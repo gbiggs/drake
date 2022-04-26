591368add92531fd480025548a6f69f54b220fa1
amcastro-tri <alejandro.castro@tri.global>
2022-04-26 17:14:36 -0400

    Revert "REVERT: adds comparison with dense matrix for debugging."
    This reverts commit a848540c2e4dcbe6b5ea0123d5d052f057478f7b.

--- multibody/contact_solvers/sap/sap_solver.cc
+++ multibody/contact_solvers/sap/sap_solver.cc
@@ -9,10 +9,6 @@
 #include "drake/common/default_scalars.h"
 #include "drake/multibody/contact_solvers/supernodal_solver.h"
 
-#include <iostream>
-#define PRINT_VAR(a) std::cout << #a": " << a << std::endl;
-#define PRINT_VARn(a) std::cout << #a":\n" << a << std::endl;
-
 namespace drake {
 namespace multibody {
 namespace contact_solvers {
@@ -111,10 +107,6 @@ SapSolverStatus SapSolver<double>::SolveWithGuess(
   model_ = std::make_unique<SapModel<double>>(&problem);
   const int nv = model_->num_velocities();
   const int nk = model_->num_constraint_equations();
-
-  PRINT_VAR(model_->num_velocities());
-  PRINT_VAR(model_->num_constraints());
-  PRINT_VAR(model_->num_constraint_equations());
 
   // Allocate the necessary memory to work with.
   auto context = model_->MakeContext();
@@ -451,13 +443,7 @@ void SapSolver<T>::CallSuperNodalSolver(const Context<T>& context,
                                         VectorX<T>* dv) const {
   if constexpr (std::is_same_v<T, double>) {
     UpdateSuperNodalSolver(context, supernodal_solver);
-    const MatrixX<double> Hsn = supernodal_solver->MakeFullMatrix();
-    const MatrixX<double> Hdense = CalcDenseHessian(context);
-    PRINT_VAR((Hsn - Hdense).norm());
-    if (!supernodal_solver->Factor()) {      
-      PRINT_VARn(Hsn);
-      PRINT_VAR(Hsn.ldlt().rcond());
-      PRINT_VAR(Hdense.ldlt().rcond());
+    if (!supernodal_solver->Factor()) {
       throw std::logic_error("SapSolver: Supernodal factorization failed.");
     }
     // We solve in place to avoid heap allocating additional memory for the
